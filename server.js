8c63f79b33885450a9217856ba3937a8abff74ce
Munther Alshawi <muntheralshawi@MacBook-Air-von-Munther.local>
2026-01-04 09:32:55 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const dotenv = require("dotenv")
+dotenv.config()
+const mongoose = require("mongoose")
+const express = require("express")
+const app = express()
+//const cors = require('cors')
+
+mongoose.connect(process.env.MONGODB_URI)
+mongoose.connection.on("connected", () => {
+    console.log("Connected to MongoBD")
+})
+
+//controllers
+
+//middleware
+const morgan = require("morgan")
+app.use(morgan("dev"))
+app.use(express.json())
+//app.use(cors())
+
+//Routes
+
+app.listen(3000, () => {
+    console.log("Express is ready")
+})
