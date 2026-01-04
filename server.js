24f0bb65c2cae49d17313a1c4505e21aad017748
Munther Alshawi <muntheralshawi@MacBook-Air-von-Munther.local>
2026-01-04 10:39:52 +0300

    classes controller help iam getting lost

--- server.js
+++ server.js
@@ -19,6 +19,10 @@ app.use(express.json())
 //app.use(cors())
 
 //Routes
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
+
 
 app.listen(3000, () => {
     console.log("Express is ready")
